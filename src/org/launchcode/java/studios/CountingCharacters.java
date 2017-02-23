6ab565d096f666bf17f2b4af4e6c81b4ea7d62cf
kevinstock <kevin@kevinstock.net>
2017-02-23 10:04:28 -0600

    CountingCharacters studio begin

---
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -0,0 +1,31 @@
+package org.launchcode.java.studios;
+
+import java.util.HashMap;
+
+/**
+ * Created by kevinstock on 2/23/17.
+ */
+public class CountingCharacters {
+
+    public static void main(String[] args) {
+        // Scanner in;
+        HashMap characters = new HashMap();
+
+        String testText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula " +
+                "scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. " +
+                "Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, " +
+                "volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+        char[] charactersInString = testText.toCharArray();
+
+        for (int i = 0; i < charactersInString.length; i++) {
+            if (characters.containsKey(charactersInString[i])) {
+                characters.put(charactersInString[i], (int)characters.get(charactersInString[i]) + 1);
+            }
+            else {
+                characters.put(charactersInString[i], 1);
+            }
+        }
+        System.out.println(characters.toString());
+    }
+}
