ca7a222ddf21bae9628e9f209557d206a6cd9e3e
kevinstock <kevin@kevinstock.net>
2017-02-23 20:32:02 -0600

    text file framework added. needs to be fixed.

--- src/org/launchcode/java/studios/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -1,5 +1,9 @@
 package org.launchcode.java.studios;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -16,10 +20,29 @@ public class CountingCharacters {
 //                "scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. " +
 //                "Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, " +
 //                "volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please provide a string: ");
-        String testText = in.nextLine();
-        testText = testText.replaceAll("[^a-zA-Z]+","");
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Please provide a string: ");
+//        String testText = in.nextLine();
+//        testText = testText.replaceAll("[^a-zA-Z]+","");
+
+        String fileName = "text.txt";
+        String line = null;
+        String testText = null;
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                testText += line;
+            }
+            bufferedReader.close();
+        } catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");
+        } catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+        System.out.println(testText);
 
         char[] charactersInString = testText.toCharArray();
 
