6daaf7d14b143da7785a96984b1f8028cddb297d
kevinstock <kevin@kevinstock.net>
2017-03-15 18:24:56 -0500

    Prep work for class 6 complete

--- src/org/launchcode/java/school/Course.java
+++ src/org/launchcode/java/school/Course.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.school;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by kevinstock on 3/11/17.
@@ -27,6 +28,32 @@ public class Course {
 
     public int getCourseCredit() {
         return courseCredit;
+    }
+
+    public boolean equals(Object o) {
+        // self check
+        if (this == o)
+            return true;
+
+        // null check
+        if (o == null)
+            return false;
+
+        // type check and cast
+        if (getClass() != o.getClass())
+            return false;
+
+        Course course = (Course) o;
+
+        // field comparison
+        return Objects.equals(courseName, course.getCourseName())
+                && getCourseCredit() == course.getCourseCredit();
+    }
+
+    public String toString() {
+        return "Course Name: " + courseName + "\n" +
+                "Credits: " + courseCredit + "\n" +
+                "Roster: " + roster.toString();
     }
 
 }
--- src/org/launchcode/java/school/Student.java
+++ src/org/launchcode/java/school/Student.java
@@ -1,20 +1,33 @@
 package org.launchcode.java.school;
 
+import java.util.Objects;
+
 /**
  * Created by kevinstock on 3/11/17.
  */
 public class Student {
 
+    private static int nextStudentId = 1;
     private String name;
     private int studentId;
     private int credits;
+    private double qualityScore = 0.0;
     private double gpa;
 
-    public Student(String name, int studentId) {
+    public Student(String name, int studentId, int credits, double gpa) {
         this.name = name;
         this.studentId = studentId;
-        this.credits = 0;
-        this.gpa = 0;
+        this.credits = credits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this (name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this (name, nextStudentId);
+        nextStudentId++;
     }
 
     public String getName() {
@@ -37,15 +50,58 @@ public class Student {
         return credits;
     }
 
-    public void setCredits(int credits) {
-        this.credits += credits;
-    }
-
     public double getGpa() {
         return gpa;
     }
 
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
+    public void addGrade(int courseCredits, double grade) {
+        this.credits += courseCredits;
+        qualityScore += (double)credits * grade;
+        gpa = (double)Math.round((qualityScore / this.credits) * 1000) / 1000;
+    }
+
+    public String getGradeLevel() {
+        // less than 30 credits is a freshman
+        if (this.getCredits() < 30) {
+            return "Freshman";
+        }
+        // 30 - 60 credits is a sophomore
+        else if (this.getCredits() >= 30 && credits < 60) {
+            return "Sophomore";
+        }
+        // 60 - 90 credits is a junior
+        else if (this.getCredits() >= 60 && credits < 90) {
+            return "Junior";
+        }
+        // 90 or more credits is a senior
+        else if (this.getCredits() >= 90) {
+            return "Senior";
+        }
+        return "Error";
+    }
+
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+
+        if (o == null)
+            return false;
+
+        if (getClass() != o.getClass())
+            return false;
+
+        Student s = (Student) o;
+
+        return Objects.equals(name, s.getName())
+                && getStudentId() == s.getStudentId();
+    }
+
+    public String toString() {
+        return "Student Name: " + name + "\n" +
+                "Student ID: " + studentId + "\n" +
+                "Credits: " + credits + "\n" +
+                "Quality Score: " + qualityScore + "\n" +
+                "GPA: " + gpa;
     }
 }
