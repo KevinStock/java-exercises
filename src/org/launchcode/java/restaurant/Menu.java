6952f1c5a1902ab26bffb4c6dece8549838226a8
kevinstock <kevin@kevinstock.net>
2017-03-15 18:27:44 -0500

    moved Menu psvm to its own class

--- src/org/launchcode/java/restaurant/Menu.java
+++ src/org/launchcode/java/restaurant/Menu.java
@@ -48,24 +48,4 @@ public class Menu {
                 '}';
     }
 
-    public static void main(String[] args) {
-        Menu breakfastMenu = new Menu("Brekfast");
-        MenuItem mi1 = new MenuItem("Pancakes", 2.75, "Stack of 3 Homestyle Pancakes", "Main");
-        System.out.println(breakfastMenu.getMenuItems().toString());
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        MenuItem mi2 = new MenuItem("Sausage", 1.50, "2 Sausage Links", "Side");
-        System.out.println(breakfastMenu.getLastUpdate());
-        breakfastMenu.addItemToMenu(mi1);
-        breakfastMenu.addItemToMenu(mi2);
-        Menu lunchMenu = new Menu("Lunch");
-        MenuItem mi3 = new MenuItem("Grilled Cheese Sandwich", 1.75, "Grilled Cheese on White Bread", "Main");
-        lunchMenu.addItemToMenu(mi3);
-        System.out.println(breakfastMenu.getMenuItems().toString());
-        System.out.println(lunchMenu.getMenuItems().toString());
-        breakfastMenu.getMenuItems().get(0).isNewItem();
-    }
 }
