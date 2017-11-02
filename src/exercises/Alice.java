46b8d472e34dcc248e026745382bfb015b2c2aee
kevinstock <kevin@kevinstock.net>
2017-11-02 17:55:02 -0500

    Class 1 Prep - Exercise 2 - Numeric Types Complete

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 /**
  * Created by kevinstock on 11/2/17.
+ * Write a program to calculate the area of a rectangle and print the answer to the console.
+ * You should prompt the user for the dimensions. (What data types should the dimensions be?)
  */
 public class Alice {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter your name: ");
-        String name = reader.next();
+        System.out.println("Enter length: ");
+        double length = reader.nextDouble();
+        System.out.println("Enter width: ");
+        double width = reader.nextDouble();
         reader.close();
-        System.out.println("Hello, " + name);
+        double area = length * width;
+        System.out.println("Area: " + area);
     }
 }
