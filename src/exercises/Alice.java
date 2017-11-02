b29ce8646bf1c99a23eb1f6e71144499f338778f
kevinstock <kevin@kevinstock.net>
2017-11-02 17:58:04 -0500

    Class 1 Prep - Exercise 3 - Numeric Types Complete

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 /**
  * Created by kevinstock on 11/2/17.
- * Write a program to calculate the area of a rectangle and print the answer to the console.
- * You should prompt the user for the dimensions. (What data types should the dimensions be?)
+ * Write a program that asks a user for the number of miles they have driven
+ * and the amount of gas they've consumed (in gallons), and print their miles-per-gallon.
  */
 public class Alice {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter length: ");
-        double length = reader.nextDouble();
-        System.out.println("Enter width: ");
-        double width = reader.nextDouble();
+        System.out.println("Enter miles: ");
+        double miles = reader.nextDouble();
+        System.out.println("Enter amount of gas in gallons consumed: ");
+        double gallons = reader.nextDouble();
         reader.close();
-        double area = length * width;
-        System.out.println("Area: " + area);
+        double mpg = miles / gallons;
+        System.out.println("MPG: " + mpg);
     }
 }
