571409fec520f15cfa442cb716d121856e91cae6
kevinstock <kevin@kevinstock.net>
2017-11-02 18:04:01 -0500

    Class 1 Prep - Exercise 4 - Strings Complete

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 
 /**
  * Created by kevinstock on 11/2/17.
- * Write a program that asks a user for the number of miles they have driven
- * and the amount of gas they've consumed (in gallons), and print their miles-per-gallon.
+ * The first sentence of Alice's Adventures in Wonderland is below.
+ * Store this sentence in a string, and then prompt the user for a term to search for within this string.
+ * Print whether or not the search term was found.
+ * See if you can make the search case-insensitive, so that searching for "alice", for example, prints true.
+ *
+ * Alice was beginning to get very tired of sitting by her sister on the bank,
+ * and of having nothing to do: once or twice she had peeped into the book her sister was reading,
+ * but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice
+ * 'without pictures or conversation?'
  */
 public class Alice {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter miles: ");
-        double miles = reader.nextDouble();
-        System.out.println("Enter amount of gas in gallons consumed: ");
-        double gallons = reader.nextDouble();
+        System.out.println("Enter search word: ");
+        String searchTerm = reader.next();
         reader.close();
-        double mpg = miles / gallons;
-        System.out.println("MPG: " + mpg);
+        String alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+        if (alice.toLowerCase().contains(searchTerm.toLowerCase())) {
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
     }
 }
